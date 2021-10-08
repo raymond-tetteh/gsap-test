4c5723c0679bbdf4f45f954eddeaf61d1932ea63
Raymond Tetteh <raymondtetteh111@gmail.com>
2021-10-08 20:59:42 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useRef} from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { Navbar, Nav, Container } from 'react-bootstrap';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -14,130 +17,154 @@ function App() {
   const boardRef = useRef();
   const boardHeaderRef = useRef();
 
-  var tl = gsap.timeline();
+  var tl = useRef();
+
+  ScrollTrigger.defaults({ scroller: ".container" });
   
   // wait until DOM has been rendered
+
+  // image 1
   useEffect(() => {
-    tl.fromTo(image1.current, {scale: 2.5}, {
+    tl.current = gsap.fromTo(image1.current, { scale: 2.5 }, {
       scrollTrigger: {
         trigger: boardHeaderRef.current,
-         endTrigger: boardRef.current,
-        toggleActions: "restart pause reverse pause",
+        endTrigger: boardRef.current,
+        toggleActions: "restart complete reverse complete",
         start: "top center",
-        end: "+=400",
+        end: "50% center",
         scrub: true,
       },
       scale: 1.0,
-      x: "39vw",
-      y: "68.7vh",
-      duration: 2,
+      y: "69.2vh",
+      x: "39.9vw",
     })
-  });
+ 
 
-  useEffect(() => {
-    tl.fromTo(image2.current, { scale: 2.5 }, {
-       scrollTrigger: {
+    // image 2
+
+    tl.current = gsap.fromTo(image2.current, { scale: 2.5 }, {
+      scrollTrigger: {
         trigger: boardHeaderRef.current,
-          endTrigger: boardRef.current,
-        toggleActions: "restart pause reverse pause",
+        endTrigger: boardRef.current,
+        toggleActions: "restart complete reverse complete",
         start: "top center",
-        end: "+=400",
+        end: "50% center",
         scrub: true,
       },
       scale: 1.0,
       y: "75.8vh",
       x: "11vw"
-     })
-  });
+    })
 
-  useEffect(() => {
-    tl.fromTo(image3.current, { scale: 2.5 }, {
-       scrollTrigger: {
+
+    // image 3
+
+    tl.current = gsap.fromTo(image3.current, { scale: 2.5 }, {
+      scrollTrigger: {
         trigger: boardHeaderRef.current,
-          endTrigger: boardRef.current,
-        toggleActions: "restart pause reverse pause",
+        endTrigger: boardRef.current,
+        toggleActions: "restart complete reverse complete",
         start: "top center",
-        end: "+=400",
+        end: "50% center",
         scrub: true,
       },
       scale: 1.0,
       y: "61.9vh",
       x: "57vw"
     })
-  });
 
-  useEffect(() => {
-    tl.fromTo(image4.current,  { scale: 2.5 }, {
+
+    //image 4
+
+    tl.current = gsap.fromTo(image4.current, { scale: 2.5 }, {
       scrollTrigger: {
         trigger: boardHeaderRef.current,
         endTrigger: boardRef.current,
-        toggleActions: "restart pause reverse pause",
+        toggleActions: "restart complete reverse complete",
         start: "top center",
-        end: "+=400",
+        end: "50% center",
         scrub: true,
       },
       scale: 1.0,
       y: "71.5vh",
       x: "10vw"
     })
-  })
+  });
 
   useEffect(() => {
-    gsap.fromTo(boardHeaderRef.current, { opacity: 0 }, {
+    gsap.fromTo(boardHeaderRef.current, { y: -200, autoAlpha: 0},{
       scrollTrigger: {
         trigger: boardRef.current,
-        toggleActions: "restart pause reverse pause",
+        toggleActions: "restart complete reverse complete",
         start: "top center",
         end: "+=120 center",
         scrub: true,
       },
-      opacity: 1,
+     y: 0,
+      autoAlpha: 1
     })
   })
 
-
   // useEffect(() => {
-  //   gsap.fromTo("#board", { backgroundSize:'100%', }, {
+  //   gsap.to(boardRef.current ,{
   //     scrollTrigger: {
-  //       trigger: boardHeaderRef.current,
+  //       trigger: image4.current,
   //       toggleActions: "restart pause reverse pause",
   //       markers: true,
-  //       start: "top center",
+  //       start: "bottom center",
   //       end: "bottom center",
   //       scrub: true,
   //     },
-  //     backgroundSize:'120%',
+  //     scale: 1.2,
   //   })
   // })
 
 
   return (
-    <div className="App">
-      <header id="blocks-header">
-         <h1>It starts with simple <span>wooden blocks</span></h1>,
-      </header>
+    <div className="App container">
+      {/* <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> */}
+     
       <section id="cards">
+        
+        <header id="blocks-header">
+         <h1>It starts with simple <span>wooden blocks</span></h1>,
+        </header>
+
         <div className="mochi-image">
-          <img id="image1" ref={image1} src='/Function_image_1.png' alt="" />
-          <img id="image2" ref={image2} src='/Function_image_2.png' alt=""/>
+          <img id="image1" ref={image1} className="board-item" src='/Function_image_1.png' alt="" />
+          <img id="image2" ref={image2} className="board-item" src='/Function_image_2.png' alt=""/>
         </div>
+
         <div className="mochi-image">
-          
+          <img id="image3" ref={image3} className="board-item" src='/Function_image_3.png' alt="" />
+          <img id="image4" ref={image4} className="board-item" src='/Function_image_4.png' alt=""/>
         </div>
-        <div className="mochi-image">
-          <img id="image3" ref={image3} src='/Function_image_3.png' alt="" />
-          <img id="image4" ref={image4} src='/Function_image_4.png' alt=""/>
-        </div>
-        <div className="mochi-image">
-   
-        </div>
+
       </section>
+
       <section id="board">
           <header id="board-header" ref={boardHeaderRef} >
-            <h1><span>Put the block on the board.</span><br/>You have written your first program</h1>,
-          </header>
-          <img className="mochi-board" ref={boardRef}  src='/Programming_board_image.png' alt=""/>
+            <h1><span>Put the block on the board.</span><br />You have written your first program</h1>,
+        </header>
+        
+        <div id="board-container" >
+          <div className="board-overlay" ref={boardRef}></div>
+          <img className="mochi-board board-item"  ref={boardRef} src='/Programming_board_image.png' alt=""/> 
+        </div>
+        
       </section>
+
     </div>
     );
   }
